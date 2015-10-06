324e13cf0a0fd77cc3d3dc2324785691f0772fe5
Carl R. Knox <knox.carl@gmail.com>
2015-10-06 15:14:08 -0700

    made project for problem 2

---
+++ problem2/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+
+// Problem 2 : Even Fibonacci numbers
+// src : https://projecteuler.net/problem=2
+
+// hold sum of even fibonacci numbers
+int totalSum;
+
+// hold numbers to add together to get next fibonacci number
+int currentFibonacciNumber, nextFibonacciNumber;
+
+
+int main() {
+    // cout << "Hello, World!" << endl;
+
+    currentFibonacciNumber = 1;
+    nextFibonacciNumber    = 2;
+
+    do {
+
+
+
+
+
+    } while (currentFibonacciNumber < 4000000);
+
+
+
+    return 0;
+}
